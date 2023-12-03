8e76606750c37ff5a4e45520088ea9feee67b93b
Reisin <zonetiki@gmail.com>
2023-12-04 04:29:06 +0700

    First commit

--- routes/UserRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createOrder } from "../controller/UserController.js";
-
-const router = express.Router();
-
-router.post("/pesan", createOrder);
-
-export default router;
